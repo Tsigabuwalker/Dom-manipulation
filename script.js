01b02f7e078b7e998e91c206d20dcb6100c1f0e9
tsigabuwalker <Seyoumwalker@gmail.com>
2025-03-22 21:19:33 +0300

    addedtheconceptof queryselector

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const test = document.body
-test.append("Hello world!")
+//const test = document.body
+//test.append("Hello world!")
 //append is used for also to display string but not appendChild
-const div = document.createElement("div")
-test.append(div)
-div.innerText = "Hello again!"
-div.textContent = "Hello world 2"
+//const div = document.createElement("div")
+//test.append(div)
+//div.innerText = "Hello again!"
+//div.textContent = "Hello world 2"
+const div = document.querySelector("div")
+console.log(div.innerText)
+console.log(div.textContent)
+
